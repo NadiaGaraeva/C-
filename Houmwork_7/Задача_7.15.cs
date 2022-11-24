6badc8fbd2af6efe77198f4d2c68d681f4c3b854
Nadia <nadyshakeri93@gmail.ru>
2022-11-24 16:17:44 +0300

    Задача 15

---
+++ Houmwork_7/Задача_7.15.cs
@@ -0,0 +1,16 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.WriteLine("Введите число в диапазоне от 1 до 7: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(DayofTheWeek(number) ? "Да" : "Нет");
+
+bool DayofTheWeek(int num)
+{
+    return (num == 6) || (num == 7);
+}
